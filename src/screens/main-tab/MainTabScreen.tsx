70b9bb6b452de7e55c2f97a66caef0f7034f3d88
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw.3005@gmail.com>
2024-04-09 08:37:18 +0630

    feat: added image loading

--- src/screens/main-tab/MainTabScreen.tsx
+++ src/screens/main-tab/MainTabScreen.tsx
@@ -89,7 +89,7 @@ export default function MainTabScreen({}: Props) {
 
   useEffect(() => {
     // setShowModal(true);
-    openLoading();
+    // openLoading();
   }, []);
 
   /* useFocusEffect(
--- src/screens/main-tab/components/MovieCard.tsx
+++ src/screens/main-tab/components/MovieCard.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, ActivityIndicator} from 'react-native';
 import React from 'react';
 import {COLORS} from 'utils/color';
 import FastImage from 'react-native-fast-image';
@@ -15,72 +15,97 @@ export default function MovieCard({
   image: string;
   rating: number;
 }) {
-  return (
-    <View
-      style={{
-        backgroundColor: COLORS.blackish_1,
-        width: '100%',
-        flexDirection: 'row',
-        height: 200,
-        borderRadius: 10,
-        overflow: 'hidden',
-        marginBottom: 10,
-      }}>
-      <FastImage
-        source={{uri: `https://image.tmdb.org/t/p/original${image}`}}
-        style={{
-          width: '35%',
-          height: '100%',
-        }}
-      />
+  const [fetchingImage, setFetchingImage] = React.useState(true);
 
+  return (
+    <>
+      {fetchingImage && (
+        <ActivityIndicator
+          size="small"
+          color={COLORS.bluish}
+          style={{
+            width: '100%',
+            flexDirection: 'row',
+            height: 200,
+            borderRadius: 10,
+            overflow: 'hidden',
+            marginBottom: 10,
+            backgroundColor: COLORS.blackish_1,
+          }}
+        />
+      )}
       <View
         style={{
-          flex: 1,
-          padding: 10,
-          gap: 10,
-          justifyContent: 'space-between',
+          backgroundColor: COLORS.blackish_1,
+          width: '100%',
+          flexDirection: 'row',
+          height: 200,
+          borderRadius: 10,
+          overflow: 'hidden',
+          marginBottom: 10,
         }}>
+        <FastImage
+          source={{
+            uri: `https://image.tmdb.org/t/p/original${image}`,
+          }}
+          style={{
+            width: 130,
+            height: 200,
+          }}
+          onLoad={() => {
+            console.log('loaded');
+            setFetchingImage(false);
+          }}
+        />
+
         <View
           style={{
-            flexDirection: 'row',
-            alignItems: 'center',
+            flex: 1,
+            padding: 10,
+            gap: 10,
             justifyContent: 'space-between',
           }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <Text
+              style={{
+                color: COLORS.bluish,
+                fontSize: 20,
+                fontWeight: 'bold',
+                width: '80%',
+              }}>
+              {title}
+            </Text>
+            <Text
+              style={{
+                color: rating >= 7 ? 'green' : 'red',
+                fontSize: 15,
+                fontWeight: 'bold',
+                borderRadius: 5,
+                padding: 5,
+                backgroundColor: COLORS.blackish_1,
+                borderColor: rating >= 7 ? 'green' : 'red',
+                borderWidth: 1,
+              }}>
+              {rating?.toFixed(1)}
+            </Text>
+          </View>
           <Text
             style={{
-              color: COLORS.bluish,
-              fontSize: 20,
-              fontWeight: 'bold',
-              width: '80%',
+              color: COLORS.greyish,
+              fontSize: 14,
+              height: 150,
+              width: '100%',
+              fontWeight: '500',
             }}>
-            {title}
-          </Text>
-          <Text
-            style={{
-              color: rating > 7 ? 'green' : 'red',
-              fontSize: 15,
-              fontWeight: 'bold',
-              borderRadius: 5,
-              padding: 5,
-              backgroundColor: COLORS.blackish_1,
-              borderColor: rating > 7 ? 'green' : 'red',
-              borderWidth: 1,
-            }}>
-            {rating?.toFixed(1)}
+            {description}
           </Text>
         </View>
-        <Text
-          style={{
-            color: COLORS.greyish,
-            fontSize: 14,
-            height: 150,
-            width: '100%',
-            fontWeight: '500',
-          }}>
-          {description}
-        </Text>
       </View>
-    </View>
+    </>
   );
 }
